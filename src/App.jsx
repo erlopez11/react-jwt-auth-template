0161a1990aed5a9b23d0bf4c894a0e430ca6e887
erlopez11 <lopezelainy@gmail.com>
2025-04-03 15:08:18 -0500

    wip:render signin route

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router';
 import NavBar from './components/NavBar/NavBar';
 import SignUpForm from './components/SignUpForm/SignUpForm';
+import SignInForm from './components/SignInForm/SignInForm';
 
 
 function App() {
@@ -12,6 +13,10 @@ function App() {
         <Route 
           path='/sign-up'
           element={<SignUpForm />}
+        />
+        <Route 
+          path='/sign-in'
+          element={<SignInForm />}
         />
       </Routes>
     </>

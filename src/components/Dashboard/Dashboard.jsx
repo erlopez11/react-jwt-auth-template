cfb589c5f362705f2c32156cfaa684b54c6b9753
erlopez11 <lopezelainy@gmail.com>
2025-04-03 16:26:29 -0500

    wip:add useeffect to show all users

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,8 +1,23 @@
-import { useContext } from 'react';
+import { useContext, useEffect } from 'react';
 import { UserContext } from '../../contexts/UserContext';
+import { index } from '../../services/userService';
 
 const Dashboard = () => {
     const { user } = useContext(UserContext);
+
+    useEffect(() => {
+        const fetchUsers = async () => {
+            try {
+                const fetchedUsers = await index();
+                console.log(fetchedUsers);
+            } catch (error) {
+                console.error(error);
+            }
+        };
+        if (user) fetchUsers();
+    }, [user]);
+
+
     return (
         <main>
             <h1>Welcome, {user.username}</h1>

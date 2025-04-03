bb704c17aa34246fbcac6aff1c4f16ec39957552
erlopez11 <lopezelainy@gmail.com>
2025-04-03 13:03:46 -0500

    wip:added user state to provider component

--- src/contexts/UserContext.jsx
+++ src/contexts/UserContext.jsx
@@ -1,11 +1,13 @@
-import { createContext } from 'react';
+import { createContext, useState } from 'react';
 
 const UserContext = createContext();
 
 //the children prop represents each component we provide context to
 const UserProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
+    const value = { user , setUser };
     return (
-        <UserContext.Provider>
+        <UserContext.Provider value={value}>
             {children}
         </UserContext.Provider>
     );
@@ -13,4 +15,5 @@ const UserProvider = ({ children }) => {
 
 export {
     UserProvider,
+    UserContext,
 }

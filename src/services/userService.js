f6b6db155ce470d063c674ae8ab710de6048ef49
erlopez11 <lopezelainy@gmail.com>
2025-04-03 16:42:33 -0500

    wip:add headers authorization

--- src/services/userService.js
+++ src/services/userService.js
@@ -2,7 +2,11 @@ const BASE_URL = `${import.meta.env.VITE_BACK_END_SERVER_URL}/users`;
 
 const index = async () => {
     try {
-        const res = await fetch(BASE_URL);
+        const res = await fetch(BASE_URL, {
+            headers: {
+                Authorization: `Bearer ${localStorage.getItem('token')}`,
+            }
+        });
         const data = await res.json();
 
         if (data.error) {

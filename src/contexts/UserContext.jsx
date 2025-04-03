746d39fbfeb39832246679023b78345bd32b9200
erlopez11 <lopezelainy@gmail.com>
2025-04-03 13:40:00 -0500

    wip:add initial state

--- src/contexts/UserContext.jsx
+++ src/contexts/UserContext.jsx
@@ -2,9 +2,15 @@ import { createContext, useState } from 'react';
 
 const UserContext = createContext();
 
+const getUserFromToken = () => {
+    const token = localStorage.getItem('token');
+    if (!token) return null; //user is probably logged out or never signed up
+    return JSON.parse(atob(token.split('.')[1])).payload;
+};
+
 //the children prop represents each component we provide context to
 const UserProvider = ({ children }) => {
-    const [user, setUser] = useState(null);
+    const [user, setUser] = useState(getUserFromToken());
     const value = { user , setUser };
     return (
         <UserContext.Provider value={value}>

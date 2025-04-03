dc2f533b83c2c6f53f3527df643bf6cd844dba9e
erlopez11 <lopezelainy@gmail.com>
2025-04-03 15:51:28 -0500

    fix: typo in UserContext & set up user

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,7 +1,8 @@
 import { useContext } from 'react';
-import { userContext } from '../../contexts/UserContext';
+import { UserContext } from '../../contexts/UserContext';
 
 const Dashboard = () => {
+    const { user } = useContext(UserContext);
     return (
         <main>
             <h1>Welcome, {user.username}</h1>
